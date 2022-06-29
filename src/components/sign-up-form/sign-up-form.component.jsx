3b19f7a7cd46846ccaeadb9f25fef26470660d6c
thangvv <70836195+thangvd2312@users.noreply.github.com>
2022-06-29 23:54:53 +0700

    Signup Coding

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: ""
+};
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+  console.log(formFields);
+  const handleChange = (event) => {
+    const { name, value } = event;
+    console.log(event);
+
+    // setFormFields({ ...formFields, [name]: value });
+  };
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          name="displayName"
+          onChange={handleChange}
+          value={displayName}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          name="email"
+          onChange={handleChange}
+          value={email}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          name="password"
+          onChange={handleChange}
+          value={password}
+        />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          name="confirmPassword"
+          onChange={handleChange}
+          value={confirmPassword}
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+export default SignUpForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,6 +21,8 @@ provider.setCustomParameters({
 });
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, provider);
 export const db = getFirestore(firebaseApp);
 export const createUserDocumentFromAuth = async (userAuth) => {
   const { uid } = userAuth;
