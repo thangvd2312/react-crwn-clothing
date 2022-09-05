ff4160c44c37ab9d25ebb57ebf5c91c828320ebd
thangvv <70836195+thangvd2312@users.noreply.github.com>
2022-09-05 16:23:13 +0700

    initial commit

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,17 +1,16 @@
-import { useContext } from "react";
+import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
-import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../store/cart/cart.selector";
 import {
   CartDropdownContainer,
   EmptyMessage,
-  CartItems,
+  CartItems
 } from "./cart-dropdown.styles";
 const CartDropdown = () => {
-  const { cartItems } = useContext(CartContext);
+  const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
 
   const goToCheckoutHandler = () => {
--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,7 +1,20 @@
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce((acc, category) => {
-        const { title, items } = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
+import { createSelector } from "reselect";
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => {
+    return categoriesSlice.categories;
+  }
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) => {
+    return categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
     }, {});
-}
+  }
+);
