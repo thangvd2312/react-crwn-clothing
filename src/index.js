99187e2acda6ebb2a3242786b0359760d264ba72
thangvv <70836195+thangvd2312@users.noreply.github.com>
2022-09-06 10:02:56 +0700

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
 import "./index.scss";
 
 const rootElement = document.getElementById("root");
@@ -12,9 +13,11 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,48 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+
 import { rootReducer } from "./root-reducer";
 
-// root-reducer
-const loggerMiddleware = (store) => (next) => (action) => {
-    console.log(action);
-    if(!action.type) {
-        return next(action);
-    }
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-    
-    next(action);
-    console.log('next state: ', store.getState());
-}
-const middleWares = [loggerMiddleware];
+// const loggerMiddleware = (store) => (next) => (action) => {
+//   console.log(action);
+//   if (!action.type) {
+//     return next(action);
+//   }
+//   console.log("type: ", action.type);
+//   console.log("payload: ", action.payload);
+//   console.log("currentState: ", store.getState());
 
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+//   next(action);
+//   console.log("next state: ", store.getState());
+// };
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// development || production
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
